3c3995fbcf441e234686eefae7ab41770880385f
sergiu-code <67414197+sergiu-code@users.noreply.github.com>
2020-07-02 18:04:16 +0300

    Delete DivideByZeroException.java

--- DivideByZeroException.java
+++
@@ -1,7 +0,0 @@
-package com.ItStep.Lesson;
-
-public class DivideByZeroException extends RuntimeException {
-    public DivideByZeroException() {
-        super("undefined result when divide by 0");
-    }
-}
